57c861395608299c3a855ed2f4ad5f4a8bcf6474
Niclas Blomberg <79707893+lambda-snail@users.noreply.github.com>
2022-02-14 21:46:32 +0100

    Added swagger metadata

---
+++ Azure.Functions/OpenApiConfigurationOptions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Abstractions;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Configurations;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Enums;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+
+public class OpenApiConfigurationOptions : DefaultOpenApiConfigurationOptions
+{
+    public override OpenApiInfo Info { get; set; } = new OpenApiInfo()
+    {
+        Version = "0.2.0",
+        Title = "GeoLocation Accelerator API",
+        Description = "Provides a simple API for making geospatial queries in Dynamics365.",
+        //TermsOfService = new Uri("https://github.com/Azure/azure-functions-openapi-extension"),
+        Contact = new OpenApiContact()
+        {
+            Name = "Niclas Blomberg",
+            Url = new Uri("https://github.com/lambda-snail/GeolocationAccelerator"),
+        },
+        License = new OpenApiLicense()
+        {
+            Name = "MIT",
+            Url = new Uri("http://opensource.org/licenses/MIT"),
+        }
+    };
+}
