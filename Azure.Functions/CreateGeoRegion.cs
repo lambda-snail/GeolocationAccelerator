9862675b9f7f621500e037adce26612697ea99db
Niclas Blomberg <79707893+lambda-snail@users.noreply.github.com>
2022-02-14 23:03:06 +0100

    Implemented request parsing logic

--- Azure.Functions/CreateGeoRegion.cs
+++ Azure.Functions/CreateGeoRegion.cs
@@ -1,6 +1,11 @@
+using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
+using Accelerator.GeoLocation.Models;
+using Accelerator.GeoLocation.Models.ViewModels;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
@@ -10,6 +15,7 @@ using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Enums;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace Accelerator.GeoLocation
 {
@@ -25,18 +31,56 @@ namespace Accelerator.GeoLocation
         [FunctionName("CreateGeoRegion")]
         [OpenApiOperation(operationId: "Run", tags: new[] { "region" }, Description = "Sync a region (polygon) consisting of multiple coordinate pairs to the database.")]
         [OpenApiSecurity("function_key", SecuritySchemeType.ApiKey, Name = "code", In = OpenApiSecurityLocationType.Query)]
-        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Description = "if the object was successfully synced with the database.")]
-        [OpenApiResponseWithBody(statusCode: HttpStatusCode.InternalServerError, contentType: "text/plain", bodyType: typeof(string), Description = "If something went wrong")]
+        [OpenApiRequestBody(contentType: "text/json", bodyType: typeof(SingleGeoPointViewModel), Description = "The CRM data to insert into the database.")]
+        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/json", bodyType: typeof(GeoRegionViewModel), Description = "if the object was successfully synced with the database.")]
+        [OpenApiResponseWithBody(statusCode: HttpStatusCode.InternalServerError, contentType: "text/json", bodyType: typeof(string), Description = "If something went wrong")]
         public async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = "georegions")] HttpRequest req)
+            [HttpTrigger(AuthorizationLevel.Function, "put", Route = "georegions")] HttpRequest req)
         {
-            
-            // TODO Implement
-            // Also need webhook
-            // Service for regions
-            // Models and viewmodels
+            try
+            {
+                string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+                JObject intermediate = JsonConvert.DeserializeObject<JObject>(requestBody);
 
-            return new OkObjectResult(string.Empty);
+                string id = ParseId(intermediate);
+                List<CoordinatePair> coordinateList = ParseCoordinateList(intermediate);
+
+                GeoRegionModel region = new(id, coordinateList);
+
+                return new OkObjectResult(region.ToString());
+            }
+            catch (ArgumentException e)
+            {
+                _logError(e);
+                return new ObjectResult(HttpStatusCode.BadRequest);
+            }
+            catch (Exception e)
+            {
+                _logError(e);
+                return new ObjectResult(HttpStatusCode.InternalServerError);
+            }
+        }
+        private void _logError(Exception e)
+        {
+            _logger.LogError(e.Message);
+            _logger.LogError(e.StackTrace);
+        }
+
+        private string ParseId(JObject jObject)
+        {
+            return jObject.Value<string>("id");
+        }
+
+        private List<CoordinatePair> ParseCoordinateList(JObject jObject)
+        {
+            JArray jsonCoordinates = jObject.Value<JArray>("coordinates");
+            List<List<double>> doubleCoordinates = jsonCoordinates.ToObject<List<List<double>>>();
+            return doubleCoordinates.Select(clist => new CoordinatePair
+                   {
+                       Longitude = clist[CoordinatePair.LongitudeIndex],
+                       Latitude = clist[CoordinatePair.LatitudeIndex]
+                   })
+                   .ToList();
         }
     }
 }
