07c3189c80e7486eea51c78fa871949e78d6921e
Niclas Blomberg <79707893+lambda-snail@users.noreply.github.com>
2022-02-24 21:46:04 +0100

    Added plugin to sync geolocations to cosmos DB (wip).

---
+++ GeoLocation.Plugins/RemoteRequestUtils.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace GeoLocation.Plugins
+{
+    public static class RemoteRequestUtils
+    {
+        /// <summary>
+        /// Send a message to the specified endpoint, using authentication in the request header.
+        /// </summary>
+        public static void SendMessage(string functionUri, string message)
+        {
+            using (System.Net.Http.HttpClient client = new System.Net.Http.HttpClient())
+            {
+                client.BaseAddress = new Uri(functionUri);
+
+                //System.Net.Http.HttpContent content = new System.Net.Http.ByteArrayContent(message);
+                System.Net.Http.HttpContent content = new System.Net.Http.StringContent(message);
+                content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/json");
+
+                var response = client.PutAsync(functionUri, content).Result;
+                if (response.IsSuccessStatusCode == false)
+                {
+                    throw new System.Net.WebException($"Error Posting to {functionUri} with code {response.StatusCode} and reason {response.ReasonPhrase}");
+                }
+            }
+        }
+    }
+}
---
+++ GeoLocation.Plugins/SyncGeoRegion.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using Microsoft.Xrm.Sdk;
+
+namespace GeoLocation.Plugins
+{
+    /// <summary>
+    /// When a location is created or updated, we put it to the geolocation db in azure.
+    /// </summary>
+    /// <remarks>The plugin assumes that the Uri including authentication code is stored in the secure configuration. For updates, it is assumed that
+    /// an image is registered with the name 'postimage' which contains the longitude and latitude.</remarks>
+    /// 
+    //[CrmPluginRegistration("Create", "niblo_geolocation",
+    //StageEnum.PostOperation, ExecutionModeEnum.Asynchronous,
+    //"niblo_longitude,niblo_latitude", "Create Step", 1, IsolationModeEnum.Sandbox,
+    //Description = "Sync geolocations to the database when created or updated.",
+    //SecureConfiguration = "Add to config file, add config file to gitignore")]
+    public sealed class SyncGeoRegion : IPlugin
+    {
+        private string serviceUri;
+
+        public SyncGeoRegion(string unsecureConfig, string secureUrl)
+        {
+            if (string.IsNullOrEmpty(secureUrl))
+            {
+                throw new InvalidPluginExecutionException("Service endpoint ID should be passed as config.");
+            }
+
+            serviceUri = secureUrl;
+        }
+
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            ITracingService tracingService = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+
+            if (!TargetIsEntity(context))
+            {
+                return;
+            }
+
+            Entity location = context.InputParameters["Target"] as Entity;
+            if(!HasLocationParameters(location))
+            {
+                return;
+            }
+
+            double longitude = 0d;
+            double latitude = 0d;
+            bool hasUpdated = false;
+            if (context.MessageName == "Create")
+            {
+                longitude = location.GetAttributeValue<double>("niblo_longitude");
+                latitude = location.GetAttributeValue<double>("niblo_latitude");
+                hasUpdated = true;
+            }
+            else if (context.MessageName == "Update")
+            {
+                if(context.PostEntityImages.Contains("postimage"))
+                {
+                    longitude = context.PostEntityImages["postimage"].GetAttributeValue<double>("niblo_longitude");
+                    latitude = context.PostEntityImages["postimage"].GetAttributeValue<double>("niblo_latitude");
+                    hasUpdated = true;
+                }
+            }
+
+            if(!hasUpdated)
+            {
+                return;
+            }
+
+            try
+            {
+
+                string message = GetSynchronizationMessage(location.Id, longitude, latitude);
+                RemoteRequestUtils.SendMessage(serviceUri, message);
+            }
+            catch (Exception e)
+            {
+                tracingService.Trace("Exception: {0}", e.ToString());
+                throw;
+            }
+        }
+
+        private bool TargetIsEntity(IPluginExecutionContext context)
+        {
+            return context.InputParameters.Contains("Target") && (context.InputParameters["Target"] is Entity);
+        }
+
+        private bool HasLocationParameters(Entity e)
+        {
+            return e.Contains("niblo_longitude") && e.Contains("niblo_latitude");
+        }
+
+        private string GetSynchronizationMessage(Guid id, double longitude, double latitude)
+        {
+            return $@"
+                {{
+                    'id': {id},
+                    'longitude': {longitude},
+                    'latitude': {latitude}
+                }}
+                ";
+        }
+    }
+}
